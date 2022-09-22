8b2a4886c37d0845128178aaf04b574a170d17c2
komad1na <nemixalone@gmail.com>
2022-09-22 18:45:18 +0200

    Bug fixes

--- src/components/weather-card/left-panel.js
+++ src/components/weather-card/left-panel.js
@@ -4,7 +4,7 @@ import getPicture from "../utils/getPicture";
 export default function LeftPanel({ data }) {
     return (
         <>
-            <div className="city-name">{data.city}</div>
+            <div className="city-name">{data.name}</div>
             <div className="icon">
                 <img className="image" src={getPicture(data.icon)} alt="icon" />
                 <div className="descriptionIcon">{data.desc}</div>
